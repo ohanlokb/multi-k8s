d15ea3fdeedb806ea1640bf251c875293c575823
Kevin <ohanlokb@hotmail.com>
2019-07-30 18:13:34 -0400

    Delete StartDashboard.cmd

--- StartDashboard.cmd
+++
@@ -1,3 +0,0 @@
-# Invoke-RestMethod -Uri https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml -Outfile kubernetes-dashboard.yaml
-# kubectl apply -f kubernetes-dashboard.yaml
-kubectl proxy
